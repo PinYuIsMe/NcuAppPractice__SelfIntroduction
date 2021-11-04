3037db23873b8392be5b32610ec073b75ebd5811
PinYuIsMe <tmfel91219pinyu@gmail.com>
2021-11-04 19:54:36 +0800

    Add a cute orange

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>大家嗨嗨~~</Text>
+      <Text style={styles.text}>我是資工大二的林品妤</Text>
+      <Text style={styles.text}>總之先在這裡放個可愛的橘子</Text>
+      <Image source={require('./assets/orange.jpg')} style={styles.img} /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5aa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#055',
+  },
+  img: {
+    borderRadius: 50,
+    resizeMode: 'contain',
+    width: 300,
+    height: 300,
+  }
 });
